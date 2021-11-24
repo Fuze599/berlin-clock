19eb36c73f0f35f06bf41804e6b3587db73ecaaa
Fuze599 <francois.bardijn@hotmail.com>
2021-11-24 16:28:23 +0100

    Test getSimpleMinutes and implementation

--- BerlinClock.php
+++ BerlinClock.php
@@ -1,1 +1,36 @@
 <?php
+class BerlinClock {
+    private int $secondes;
+    private int $minutes;
+    private int $heures;
+
+    public function __construct($secondes, $minutes, $heures) {
+        $this->secondes = $secondes;
+        $this->minutes = $minutes;
+        $this->heures = $heures;
+    }
+
+    public function getSimpleMinutes() : int {
+        return $this->minutes % 5;
+    }
+
+    public function getFiveMinutes() : int {
+        return 0;
+    }
+
+    public function getSimpleHours() : int {
+        return 0;
+    }
+
+    public function getFiveHours() : int {
+        return 0;
+    }
+
+    public function getSeconds() : int {
+        return 0;
+    }
+
+    public function horloge() : string {
+        return "";
+    }
+}
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,1 +1,44 @@
 <?php
+use PHPUnit\Framework\TestCase;
+require_once('BerlinClock.php');
+
+class BerlinClockTest extends TestCase {
+    private BerlinClock $berlinClock;
+
+    protected function setUp(): void {
+        parent::setUp();
+    }
+
+    public function testGetSimpleMinutesShouldReturn0() {
+        // Arrange
+        $berlinClock = new BerlinClock(1, 5, 1);
+
+        // Act
+        $response = $berlinClock->getSimpleMinutes();
+
+        // Assert
+        self::assertEquals(0, $response);
+    }
+
+    public function testGetSimpleMinutesShouldReturn1() {
+        // Arrange
+        $berlinClock = new BerlinClock(1, 1, 1);
+
+        // Act
+        $response = $berlinClock->getSimpleMinutes();
+
+        // Assert
+        self::assertEquals(1, $response);
+    }
+
+    public function testGetSimpleMinutesShouldReturn1Bis() {
+        // Arrange
+        $berlinClock = new BerlinClock(1, 6, 1);
+
+        // Act
+        $response = $berlinClock->getSimpleMinutes();
+
+        // Assert
+        self::assertEquals(1, $response);
+    }
+}
